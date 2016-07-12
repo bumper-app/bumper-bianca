6124084ea6cad4ea68f09f6d67e935847ee32b00
Mathieu Nayrolles <mathieu.nayrolles@gmail.com>
2016-07-12 13:03:59 -0400

    new setup

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ import setuptools
 from packagename.version import Version
 
 
-setuptools.setup(name='py-boilerplate',
-                 version=Version('1.0.0').number,
-                 description='Python Package Boilerplate',
+setuptools.setup(name='py-bianca',
+                 version=Version('0.0.1').number,
+                 description='TBA',
                  long_description=open('README.md').read().strip(),
-                 author='Package Author',
-                 author_email='you@youremail.com',
-                 url='http://path-to-my-packagename',
-                 py_modules=['packagename'],
+                 author='Mathieu Nayrolles',
+                 author_email='mathieu.nayrolles@gmail.com',
+                 url='https://github.com/bumper-app/bumper-bianca',
+                 py_modules=['py-bianca'],
                  install_requires=[],
                  license='MIT License',
                  zip_safe=False,
-                 keywords='boilerplate package',
-                 classifiers=['Packages', 'Boilerplate'])
+                 keywords='tba',
+                 classifiers=['tba'])
