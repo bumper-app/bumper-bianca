9837276d110c630bbed57dac885448485de42f4a
Mathieu Nayrolles <mathieu.nayrolles@gmail.com>
2016-07-14 11:15:09 -0400

    first working API endpoint for repos

--- bianca/app.py
+++ bianca/app.py
@@ -1,10 +1,43 @@
-from flask import Flask
+from flask import Flask, jsonify, request, abort, make_response
+from analyzer.analyzer import *
+from ingester.ingester import *
+from orm.repository import *
+import calendar # to convert datetime to unix time
+from caslogging import logging
+from queue import *
+import threading
+import time
+from monthdelta import *
 
 app = Flask(__name__)
+session = Session()
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return "Nothing to see"
+
+@app.route('/api/v1.0/repos', methods=['POST'])
+def create_repo():
+    if not request.json or not 'url' in request.json:
+        abort(400)
+
+    repos = (session.query(Repository) 
+	.filter_by(url = request.json['url'])
+	.all())
+
+    repo = None
+
+    print(request.json['url']);
+
+    if len(repos) == 0:
+        print("hi")
+        repo = Repository(url=request.json['url'])
+        session.add(repo)
+        session.commit()
+    else:
+        repo = repos[0]
+
+    return jsonify(repo.as_dict()), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
